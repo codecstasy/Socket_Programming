2c1f27104322b1277097b849242acdbf200091cb
Shattik Bandyopadhyaa <codecstasy.shattik@gmail.com>
2023-09-20 00:00:12 +0600

    Thread pool used to handle multiple clients

---
+++ src/Thread_Pool/Client.java
@@ -0,0 +1,35 @@
+package Thread_Pool;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    private static final String HOST = "localhost";
+    private static final int PORT = 8080;
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Socket clientSocket = new Socket(HOST, PORT);
+        System.out.println("Client connected to server: " + clientSocket.getRemoteSocketAddress());
+
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+//        PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(clientSocket.getOutputStream());
+        ObjectInputStream objectInputStream = new ObjectInputStream(clientSocket.getInputStream());
+
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            String message = sc.nextLine();
+            if(message.equals("Exit"))
+                break;
+            // Write a message to the server
+            objectOutputStream.writeObject(message);
+
+            // Reading the response from server
+            String response = (String)objectInputStream.readObject();
+            System.out.println("Response from server: " + response);
+        }
+
+        clientSocket.close();
+    }
+}
---
+++ src/Thread_Pool/Server.java
@@ -0,0 +1,58 @@
+package Thread_Pool;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    private static final int PORT = 8080;
+
+    public static void main(String[] args) throws IOException {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);    // Using Fixed sized thread pool
+
+        ServerSocket serverSocket = new ServerSocket(PORT);
+        System.out.println("Server started at port " + PORT);
+
+        while(true) {
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Client connected: " + clientSocket.getRemoteSocketAddress());
+
+            // Create a new thread to handle the client connection
+            executorService.execute(new ClientHandler(clientSocket));
+        }
+    }
+}
+
+class ClientHandler implements Runnable {
+    private final Socket clientSocket;
+    public ClientHandler(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    public Socket getClientSocket() {
+        return clientSocket;
+    }
+
+    public void run() {
+        try {
+//            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+//            PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(clientSocket.getOutputStream());
+            ObjectInputStream objectInputStream = new ObjectInputStream(clientSocket.getInputStream());
+
+            while(true) {
+                // Reading the message from client
+                String message = (String) objectInputStream.readObject();
+                if(message == null)
+                    break;
+                // Echoing the message back to client
+                objectOutputStream.writeObject("Echo: " + message.toUpperCase());
+            }
+//            clientSocket.close();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
